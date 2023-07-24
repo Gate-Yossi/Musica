8d300e72652f644411c708365119a3a740438bc9
Gate-Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-07-24 21:22:58 +0900

    note 2023.08.02

--- 18_learn_terraform-aws-ec2/security.tf
+++ 18_learn_terraform-aws-ec2/security.tf
@@ -4,15 +4,6 @@ resource "aws_security_group" "learn_ec2_sg" {
   vpc_id = aws_vpc.learn_ec2_vpc.id
 }
 
-resource "aws_security_group_rule" "learn_ec2_ingress" {
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.learn_ec2_sg.id
-}
-
 resource "aws_security_group_rule" "learn_ec2_ingress_http" {
   type              = "ingress"
   from_port         = "80"
