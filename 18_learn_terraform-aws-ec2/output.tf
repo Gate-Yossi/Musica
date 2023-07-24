0972d0e9612d4554435c8392ea6e989098b7f083
Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-07-24 10:48:01 +0900

    2023.08.01 投稿 (#30)
    * note 2023.08.01
    
    * modify README

--- 18_learn_terraform-aws-ec2/output.tf
+++ 18_learn_terraform-aws-ec2/output.tf
@@ -4,6 +4,11 @@ output "ec2_global_ips" {
   value = aws_instance.learn_ec2_instance.*.public_ip
 }
 
+# 作成したEC2のインスタンスIDを出力
+output "ec2_instance_id" {
+  value = aws_instance.learn_ec2_instance.*.id
+}
+
 output "secretsmanager_secret" {
   value = aws_secretsmanager_secret.learn_ec2_secret.id
 }
